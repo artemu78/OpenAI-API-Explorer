a91ff08606e13f0d2a9e5dad06cb8e36c34ec153
ArtemReva <artemreva79@gmail.com>
2024-10-02 15:51:16 +0300

    v1.4.44 Fix build script

--- build.js
+++ build.js
@@ -1,12 +1,25 @@
 const { spawn } = require("child_process");
 const fs = require('fs');
+const path = require('path');
+
+const BUILD_FOLDER = 'dist';
+
+function prepareFolder() {
+  const buildPath = path.join(__dirname, BUILD_FOLDER);
+  if (!fs.existsSync(buildPath)) {
+    fs.mkdirSync(buildPath);
+  }
+  return buildPath;
+}
 
 function runEsbuild() {
   console.log("Run build");
-  runProcess("options.ts", "dist/options.js");
-  runProcess("content.ts", "dist/content.js");
-  runProcess("background.ts", "dist/out.js");
-  fs.copyFileSync("options.html", "dist/options.html");
+  const buildPath = prepareFolder();
+
+  runProcess("options.ts", path.join(buildPath, "options.js"));
+  runProcess("content.ts", path.join(buildPath, "content.js"));
+  runProcess("background.ts", path.join(buildPath, "out.js"));
+  fs.copyFileSync("options.html", path.join(buildPath, "options.html"));
   spawn("node", ["archive"]);
 }
 
